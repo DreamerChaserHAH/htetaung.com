c059fe8b5f133af5a8c54996d9a22adc2f552880
Ethan <contact@htetaung.com>
2024-05-24 07:12:15 +0800

    fix: wrong ratio

--- src/main.cpp
+++ src/main.cpp
@@ -207,7 +207,7 @@ int main()
     float X = resolutionX > resolutionY? resolutionX : resolutionY;
     float Y = resolutionX > resolutionY? resolutionY : resolutionX;
     
-    float factor = std::clamp(X/Y, X/Y, 100.0f);
+    float factor = std::clamp(X/Y, static_cast<float>(INITIAL_SCREEN_WIDTH)/INITIAL_SCREEN_HEIGHT, 100.0f);
 
     SCREEN_HEIGHT = INITIAL_SCREEN_HEIGHT;
     SCREEN_WIDTH = INITIAL_SCREEN_HEIGHT * factor;
