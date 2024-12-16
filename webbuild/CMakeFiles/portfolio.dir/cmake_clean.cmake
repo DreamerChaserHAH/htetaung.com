42f2779c85e974c99b7f5b275035ea47dfaabcf4
Victor Mak <contact@htetaung.com>
2024-12-16 11:03:48 +0800

    feat: realign text

---
+++ webbuild/CMakeFiles/portfolio.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/portfolio.dir/src/main.cpp.o"
+  "CMakeFiles/portfolio.dir/src/main.cpp.o.d"
+  "portfolio.html"
+  "portfolio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/portfolio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ webbuild/CMakeFiles/projects.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projects.dir/src/projects.cpp.o"
+  "CMakeFiles/projects.dir/src/projects.cpp.o.d"
+  "libprojects.a"
+  "libprojects.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
